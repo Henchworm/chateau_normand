86ff7f5dc2ee2dc3fcd1897d9cb627111d7de06c
chewittbeyond <104784564+chewittbeyond@users.noreply.github.com>
2022-09-16 22:22:24 -0600

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,13 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
