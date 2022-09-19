9cdd761abed80ae37508489046626129faf93f45
chewittbeyond <104784564+chewittbeyond@users.noreply.github.com>
2022-09-18 20:36:05 -0600

    mount goodjob

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   root to: 'admin/dashboard#index'
   ActiveAdmin.routes(self)
+
+  mount GoodJob::Engine => 'good_job'
+
   # post ‘/twilio/sms’
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
